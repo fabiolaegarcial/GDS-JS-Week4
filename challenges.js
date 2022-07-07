3382b544f83955f035137bb0b265dc6e0b826794
fabiolaegarcial <67425045+fabiolaegarcial@users.noreply.github.com>
2022-07-07 15:34:54 -0500

    finished very easy and easy tasks

--- challenges.js
+++ challenges.js
@@ -1,1 +1,20 @@
+// VERY EASY 
+function min(val1, val2){
+    return Math.min(val1, val2);
+}
 
+// EASY
+//create an array of students holding their last, first, and age
+// [0] lastName  -  [1] firstName  -  [2] age
+var student1 = ["Doe", "John", "20"];
+var student2 = ["Smith", "Jane", "19"];
+var student3 = ["Thompson", "Jack", "21"];
+var studentArray = [student1, student2, student3];
+
+console.log(`Hello, my name is ${studentArray[1][1]} ${studentArray[1][0]} and I'm ${studentArray[1][2]} years old.`);
+
+// MEDIUM
+
+
+
+// HARD
